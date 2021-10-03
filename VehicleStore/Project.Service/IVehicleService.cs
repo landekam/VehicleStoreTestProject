58f2976b2449b5621f2f3ac95eb5d1fa37da0a42
landekam <matejlandeka2@gmail.com>
2021-10-03 19:23:52 +0200

    Created VehicleService interface and paging, sorting and filtering interfaces.

---
+++ VehicleStore/Project.Service/IVehicleService.cs
@@ -0,0 +1,17 @@
+﻿using Project.Service.Model.Interface;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.Service
+{
+    public interface IVehicleService
+    {
+        Task<IEntity> CreateAsync(IEntity entity);
+        Task<IEntity> ReadAsync(Guid id);
+        Task<int> UpdateAsync(IEntity entity);
+        Task<int> DeleteAsync(Guid id);
+        Task<IEnumerable<IEntity>> FindAsync(ISort sort, IFilter filter, IPage page);
+    }
+}
